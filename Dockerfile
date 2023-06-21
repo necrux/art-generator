f79c41f6323ed2551202af8ed3f46be9e7bf5b82
Wes Henderson <whendersonii@gmail.com>
2023-06-20 19:10:34 -0500

    Staring to mess with ChatGPT.

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM python:3.11
 EXPOSE 5000
 WORKDIR /app
+COPY .keys .
 COPY requirements.txt .
 RUN pip install -r requirements.txt
 COPY ./src/art-generator .
--- src/art-generator/resources/art.py
+++ src/art-generator/resources/art.py
@@ -1,8 +1,17 @@
+import configparser
+import openai
 import requests
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 
 blp = Blueprint("Resume", __name__, description="CSS Art Generator")
+
+AUTH_FILE = "/app/.keys"
+
+config = configparser.RawConfigParser()
+config.read(AUTH_FILE)
+
+api_key = config["openai"]["key"]
 
 
 @blp.route("/")
@@ -11,35 +20,33 @@ class Welcome(MethodView):
         return {"message": "Welcome. Be sure to review the Swagger docs: /swagger-ui"}
 
 
-@blp.route("/resume")
-class Resume(MethodView):
-    def get(self):
-        return resume
+#@blp.route("/resume")
+#class Resume(MethodView):
+#    def get(self):
+#        return resume
+#
+#    def delete(self):
+#        global resume
+#        resume = {}
+#        return resume
+#
+#
+#@blp.route("/resume/refresh")
+#class Refresh(MethodView):
+#    def get(self):
+#        global resume
+#        try:
+#            response = requests.get(url=RESUME_ENDPOINT)
+#            response.raise_for_status()
+#        except requests.exceptions.HTTPError:
+#            abort(500, message="Failed to update resume content. Please try again.")
+#        else:
+#            resume = yaml.safe_load(response.text)
+#            return "Grabbed the latest resume."
 
-    def delete(self):
-        global resume
-        resume = {}
-        return resume
 
-
-@blp.route("/resume/refresh")
-class Refresh(MethodView):
-    def get(self):
-        global resume
-        try:
-            response = requests.get(url=RESUME_ENDPOINT)
-            response.raise_for_status()
-        except requests.exceptions.HTTPError:
-            abort(500, message="Failed to update resume content. Please try again.")
-        else:
-            resume = yaml.safe_load(response.text)
-            return "Grabbed the latest resume."
-
-
-@blp.route("/resume/<string:section>")
+@blp.route("/art/v1/<string:subject>/<string:style>")
 class Section(MethodView):
-    def get(self, section):
-        try:
-            return resume[section.title()]
-        except KeyError:
-            abort(404, message=f"No section found: {section}")
+    def get(self, subject, style):
+        prompt = f"Generate a {subject} in a {style} style using purely HTML and CSS."
+        return api_key
